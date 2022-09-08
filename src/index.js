abd8476c60a5e54f4d04e66646e7126a8af6d133
alenak26 <108095300+alenak26@users.noreply.github.com>
2022-09-08 14:09:28 +0200

    built project based on given example solution

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import WeatherSearch from "./WeatherSearch";
+
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-    </div>
-    <footer>
-      <a href="https://github.com/alenak26/weather_react"> Open-sorce code</a>{" "}
-      by Olena Koloshynska
-    </footer>
+    <App />
   </StrictMode>
 );
